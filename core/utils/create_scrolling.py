0f0b30b38dcc2acda800511ed7b0da77430b01b1
VitaliyVorobyov <123983999+VitaliyVorobyov@users.noreply.github.com>
2024-03-05 22:09:04 +0300

    bag fix: 'error count scroll'

--- core/utils/create_scrolling.py
+++ core/utils/create_scrolling.py
@@ -3,10 +3,10 @@ async def scrolling(content: list, start: int, request):
         start = 1
     if start < 1:
         start = len(content)
-    content_dict = {y: x for x, y in enumerate([x['file_to_description_id'] for x in content], start=1)}
+    content_dict = {x: y for x, y in enumerate([x['file_to_description_id'] for x in content], start=1)}
     first_content = content_dict.get(start)
     file_to_description = await request.get_file_to_description(first_content)
-    scroll_data = [first_content, len(content_dict)]
+    scroll_data = [start, len(content_dict)]
     row_id = file_to_description[0]['row_id']
     description_id = file_to_description[0]['description_id']
     description = await request.get_description(description_id)
