49d80eee19cb3c369f04fcf2551c7bbd58b94116
VitaliyVorobyov <123983999+VitaliyVorobyov@users.noreply.github.com>
2024-03-07 00:51:22 +0300

    bag fix: 'delete constant from table "Buttons"'

--- core/handlers/statistics.py
+++ core/handlers/statistics.py
@@ -3,12 +3,10 @@ from aiogram.types import CallbackQuery
 
 from core.keyboards.inline import keyboard_back
 from core.utils.callbackdata import KeyboardAdmin
-from core.utils.dbconnect import Request
 router = Router()
 
 
 @router.callback_query(KeyboardAdmin.filter(F.name == 'statistics'))
-async def statistics(call: CallbackQuery, bot: Bot, request: Request):
-    await request.drop_unique_constraint()
+async def statistics(call: CallbackQuery, bot: Bot):
     await bot.edit_message_caption(call.from_user.id, call.message.message_id, caption=f'Раздел пуст',
                                    reply_markup=keyboard_back())
